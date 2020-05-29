8f74497b26999423b3df5c81215a8776ab4b3ce6
Magesh Kuppan <tkmagesh77@gmail.com>
2020-05-29 09:59:24 +0530

    file read using streams

--- 04-print-file/print-file-async.js
+++ 04-print-file/print-file-async.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 
+//Error First Callback Pattern
 fs.readFile('./sample1.txt', { encoding: 'utf8'}, function(e, fileContents){
     if (e){
         console.log('something went wrong');
---
+++ 04-print-file/print-file-stream.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+
+var stream = fs.createReadStream('./sample.txt', { encoding: 'utf8' });
+
+//readable stream events -> open, data, end, close, error
+var readCount = 0;
+
+stream.on('data', function(chunk) {
+    ++readCount;
+    console.log(chunk);
+});
+
+stream.on('end', function(){
+    console.log('Thats all folks');
+    console.log(readCount, ' chunks are read');
+});
+
+stream.on('error', function(e){
+    console.log('something went wrong');
+    console.log(e);
+});
