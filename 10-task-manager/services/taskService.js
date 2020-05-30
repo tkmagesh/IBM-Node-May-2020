3749e5ac80e736913317c71355f6a7e2adae1131
Magesh Kuppan <tkmagesh77@gmail.com>
2020-05-30 14:54:09 +0530

    Day-02 complete

--- 10-task-manager/services/taskService.js
+++ 10-task-manager/services/taskService.js
@@ -1,28 +1,30 @@
-var taskList = [
-    { id : 1, name : 'Master JavaScript', isCompleted : false},
-    { id: 2, name: 'Explore Node.js', isCompleted: true }
-];
 
 function getAll(){
+    var taskList = //read from the taskDb.json file
     return taskList;
 }
 
 function get(id){
+    var taskList = //read from the taskDb.json file
     return taskList.find(task => task.id === id);
 }
 
 function save(taskData){
+    var taskList = //read from the taskDb.json file
     if (taskData.id === 0){
         taskData.id = taskList.reduce((result, task) => result > task.id ? result : task.id, 0) + 1;
         taskList.push(taskData);
+        //write the taskList to the taskDb.json file
     } else {
         taskList = taskList.map(task => task.id === taskData.id ? taskData : task);
+        //write the taskList to the taskDb.json file
     }
     return taskData;
 }
 
 function remove(id){
     taskList = taskList.filter(task => task.id !== id);
+    //write the taskList to the taskDb.json file
     return;
 }
 
